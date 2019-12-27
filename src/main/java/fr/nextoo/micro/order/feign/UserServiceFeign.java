a5808d49956a3330f9dbc32d7484f956d4dfe77e
Alexandre PLATEL <aplatel@nextoo.fr>
2019-12-27 15:57:46 +0100

    mise en place architecture pour User et Order

--- src/main/java/fr/nextoo/micro/order/feign/UserServiceFeign.java
+++ src/main/java/fr/nextoo/micro/order/feign/UserServiceFeign.java
@@ -3,5 +3,5 @@ package fr.nextoo.micro.order.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "USER-SERVICE")
-public class UserServiceFeign {
+public interface UserServiceFeign {
 }
